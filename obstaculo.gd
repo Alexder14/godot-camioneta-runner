78cf7f3fd50b1fd64f634c5c8c87a4aa19c0167c
Alexander Salazar <1817571250101@ingenieria.usac.edu.gt>
2025-11-30 08:38:38 -0600

    Día 4 Completado: Detección de colisiones y Game Over

--- obstaculo.gd
+++ obstaculo.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
-# Velocidad de caída (igual que la carretera para que se vea natural)
+# Velocidad de caída
 const VELOCIDAD = 300.0
 
+# FUNCIÓN 1: MOVIMIENTO (Lo que ya tenías)
 func _process(delta):
-	# Mover hacia abajo
 	position.y += VELOCIDAD * delta
 	
-	# Si baja mucho (800px), lo borramos para liberar memoria
+	# Borrar si sale de la pantalla
 	if position.y > 800:
 		queue_free()
+
+# FUNCIÓN 2: DETECTAR CHOQUE (¡ESTA FALTABA!)
+# Esta es la que detiene el juego.
+func _on_body_entered(body):
+	print("ALGO ME TOCÓ: ", body.name) # Chismoso
+	
+	if body.name == "Jugador":
+		print("💥 ¡CHOCASTE! GAME OVER 💥")
+		get_tree().paused = true
